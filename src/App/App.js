f6461581a6ece18c266b58e96a39e8d81a07aec1
Siwadon Saosoong <siwadon.s@gmail.com>
2018-04-13 17:31:12 -0700

    Add simple routing using react-router

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Chart from '../Chart';
 import { csv } from 'd3-fetch';
 import Trip from '../Trip';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import logo from '../bike_white_48px.svg';
@@ -16,23 +17,35 @@ class App extends Component {
   }
 
   async componentWillMount() {
-    const trips = await csv(
-      '../data/ford_gobike/mini.csv',
-    );
+    const trips = await csv('../data/ford_gobike/mini.csv');
 
     this.setState({ data: trips.map(Trip) });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bike Sharing</h1>
-        </header>
-        <p className="App-intro" />
-        <Chart data={this.state.data} />
-      </div>
+      <BrowserRouter>
+        <div>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Bike Sharing</h1>
+            </header>
+            <p className="App-intro" />
+          </div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/debug">Debug</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={null} />
+            <Route
+              path="/debug"
+              render={() => <Chart data={this.state.data} />}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
