89c5388ddd639a15f31119a5fd3a11a8ea49294f
Siwadon Saosoong <siwadon.s@gmail.com>
2018-04-25 10:52:18 -0700

    Add tooltip for Matrix

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import * as d3 from "d3";
+import * as d3 from 'd3';
 
 class Matrix extends Component {
   constructor(props) {
@@ -23,8 +23,7 @@ class Matrix extends Component {
   createChart() {
     const { stations } = this.props;
     const svg = d3.select(this.node);
-    const width = svg.attr("width");
-    const height = svg.attr("height");
+    const width = svg.attr('width');
     const matrix = [];
     const xScale = d3.scaleBand().range([0, width]);
     const c = d3.scaleOrdinal(d3.schemeCategory10).domain(d3.range(10));
@@ -36,7 +35,7 @@ class Matrix extends Component {
     this.props.trips.forEach((t, i) => {
       matrix.push({
         x: stations.findIndex(e => e.id === t.start_station.id),
-        y: stations.findIndex(e => e.id === t.end_station.id)
+        y: stations.findIndex(e => e.id === t.end_station.id),
       });
     });
 
@@ -55,91 +54,115 @@ class Matrix extends Component {
 
     xScale.domain(stations.map(e => e.id));
 
+    const tooltip = d3
+      .select('body')
+      .append('div')
+      .attr('class', 'tooltip');
+
     var column = svg
-      .selectAll(".column")
+      .selectAll('.column')
       .data(stations)
       .enter()
-      .append("g")
-      .attr("class", "column")
-      .attr("transform", function(d) {
-        return "translate(" + xScale(d.id) + ") rotate(-90)";
+      .append('g')
+      .attr('class', 'column')
+      .attr('transform', function(d) {
+        return 'translate(' + xScale(d.id) + ') rotate(-90)';
       });
 
-    column.append("line").attr("x1", -width);
+    column.append('line').attr('x1', -width);
 
     column
-      .append("text")
-      .attr("x", 6)
-      .attr("y", xScale.bandwidth())
-      .attr("dy", ".32em")
-      .attr("text-anchor", "start")
+      .append('text')
+      .attr('x', 6)
+      .attr('y', xScale.bandwidth())
+      .attr('dy', '.32em')
+      .attr('text-anchor', 'start')
       .text(function(d, i) {
         return d.name;
       })
-      .style("font-size", "3px");
+      .style('font-size', '3px');
 
     var row = svg
-      .selectAll(".row")
+      .selectAll('.row')
       .data(group_trips)
       .enter()
-      .append("g")
-      .attr("class", "row")
-      .attr("transform", function(d) {
-        return "translate(0," + xScale(stations[d.key].id) + ")";
+      .append('g')
+      .attr('class', 'row')
+      .attr('transform', function(d) {
+        return 'translate(0,' + xScale(stations[d.key].id) + ')';
       })
       .each(get_row);
 
-    row.append("line").attr("x2", width);
+    row.append('line').attr('x2', width);
 
     row
-      .append("text")
-      .attr("x", -6)
-      .attr("y", xScale.bandwidth())
-      .attr("dy", ".32em")
-      .attr("text-anchor", "end")
+      .append('text')
+      .attr('x', -6)
+      .attr('y', xScale.bandwidth())
+      .attr('dy', '.32em')
+      .attr('text-anchor', 'end')
       .text(function(d, i) {
         return stations[d.key].name;
       })
-      .style("font-size", "3px");
+      .style('font-size', '3px');
 
     function get_row(value) {
       var cell = d3.select(this);
       cell
-        .selectAll(".cell")
+        .selectAll('.cell')
         .data(value.values)
         .enter()
-        .append("rect")
-        .attr("class", "cell")
-        .attr("x", function(d) {
+        .append('rect')
+        .attr('class', 'cell')
+        .attr('x', function(d) {
           return xScale(stations[d.key].id);
         })
-        .attr("width", xScale.bandwidth())
-        .attr("height", xScale.bandwidth())
-        .style("fill-opacity", function(d) {
+        .attr('width', xScale.bandwidth())
+        .attr('height', xScale.bandwidth())
+        .style('fill-opacity', function(d) {
           return z(d.value);
         })
-        .style("fill", function(d) {
+        .style('fill', function(d) {
           return c(d.value);
         })
-        .on("mouseover", function(d) {
-          mouseover(value.key, stations[d.key].id);
-        })
-        .on("mouseout", mouseout);
+        .on('mouseover', d => handleMouseOver(d, value.key))
+        .on('mouseout', handleMouseOut)
+        .on('mousemove', handleMouseMove);
     }
 
-    function mouseover(x, y) {
-      d3.selectAll(".column text").classed("active", function(d, i) {
-        return stations[i].id === y;
+    function handleMouseOver(d, key) {
+      let text = '';
+      const columnId = parseInt(d.key, 10);
+
+      const columnSelector = `.column:nth-child(${columnId})`;
+      d3
+        .select(columnSelector)
+        .select('text')
+        .classed('active', true);
+
+      d3.selectAll('.row text').classed('active', function(e, i) {
+        return e.key === key;
       });
-      d3.selectAll(".row text").classed("active", function(d, i) {
-        return d.key === x;
-      });
+
+      text += `${stations[columnId].name}<br>${stations[key].name}<br/><br/>${
+        d.value
+      }`;
+
+      tooltip.html(text);
+      tooltip.style('visibility', 'visible');
     }
 
-    function mouseout() {
-      d3.selectAll("text").classed("active", false);
-      d3.selectAll("rect").attr("width", xScale.bandwidth());
-      d3.selectAll("rect").attr("height", xScale.bandwidth());
+    function handleMouseOut(d) {
+      d3.selectAll('text').classed('active', false);
+      d3.selectAll('rect').attr('width', xScale.bandwidth());
+      d3.selectAll('rect').attr('height', xScale.bandwidth());
+      tooltip.style('visibility', 'hidden');
+    }
+
+    function handleMouseMove(d) {
+      tooltip
+        .style('top', d3.event.pageY - 32 + 'px')
+        .style('left', d3.event.pageX + 8 + 'px');
     }
   }
 
@@ -157,7 +180,7 @@ class Matrix extends Component {
 
 Matrix.propTypes = {
   trips: PropTypes.array.isRequired,
-  stations: PropTypes.array.isRequired
+  stations: PropTypes.array.isRequired,
 };
 
 export default Matrix;
