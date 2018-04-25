4fe6ea775c01cf495ebaf1a9d4e3eceba5fd9489
Surada Lerkpatomsak <slerkpatomsak@usfca.edu>
2018-04-25 10:59:41 -0700

    update interactive

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import Checkbox from "material-ui/Select";
+import { FormGroup, FormControlLabel } from "material-ui/Form";
+import Grid from "material-ui/Grid";
+
 import * as d3 from "d3";
 
 class Matrix extends Component {
@@ -52,6 +56,15 @@ class Matrix extends Component {
         return v.length;
       })
       .entries(matrix);
+
+    var orders = {
+      name: d3.range(stations.length).sort(function(a, b) {
+        return d3.ascending(stations[a].name, stations[b].name);
+      })
+    };
+    // for (var i = 0; i < stations.length; i++) {
+    //   console.log(stations[orders.name[i]].name);
+    // }
 
     xScale.domain(stations.map(e => e.id));
 
@@ -141,6 +154,50 @@ class Matrix extends Component {
       d3.selectAll("rect").attr("width", xScale.bandwidth());
       d3.selectAll("rect").attr("height", xScale.bandwidth());
     }
+
+    // d3.select("#order").on("change", function() {
+    //   clearTimeout(timeout);
+    //   order(this.value);
+    // });
+
+    // function order(value) {
+    //   xScale.domain(orders.name);
+    //   var t = svg.transition().duration(1500);
+
+    //   t
+    //     .selectAll(".row")
+    //     .delay(function(d, i) {
+    //       return xScale(stations[d.key].id) * 4;
+    //     })
+    //     .attr("transform", function(d, i) {
+    //       return "translate(0," + xScale(stations[d.key].id) + ")";
+    //     })
+    //     .selectAll(".cell")
+    //     .delay(function(d) {
+    //       return xScale(stations[d.key].id) * 4;
+    //     })
+    //     .attr("x", function(d) {
+    //       return xScale(stations[d.key].id);
+    //     });
+
+    //   t
+    //     .selectAll(".column")
+    //     .delay(function(d, i) {
+    //       return xScale(d.id) * 4;
+    //     })
+    //     .attr("transform", function(d, i) {
+    //       return "translate(" + xScale(d.id) + ")rotate(-90)";
+    //     });
+    // }
+
+    // var timeout = setTimeout(function() {
+    //   order("group");
+    //   d3
+    //     .select("#order")
+    //     .property("selectedIndex", 2)
+    //     .node()
+    //     .focus();
+    // }, 2000);
   }
 
   render() {
