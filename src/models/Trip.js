578b43ec114aa4a7aa5302d6a247685c08b1053c
Siwadon Saosoong <siwadon.s@gmail.com>
2018-04-30 15:35:21 -0700

    Fix station indices

--- src/models/Trip.js
+++ src/models/Trip.js
@@ -3,10 +3,10 @@ const Trip = (obj, stations) => ({
   duration: +obj.duration_sec,
 
   // TODO: Timezone is not correct in Safari
-  start_station: stations[+obj.start_station_id],
+  start_station: stations.find(e => e.id === +obj.start_station_id),
   start_time: new Date(obj.start_time.replace(' ', 'T')),
 
-  end_station: stations[+obj.end_station_id],
+  end_station: stations.find(e => e.id === +obj.end_station_id),
   end_time: new Date(obj.end_time.replace(' ', 'T')),
 
   user_type: obj.user_type,
