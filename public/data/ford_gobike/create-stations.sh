34258986ef1bfd8beace0917f4ae15421bc5a0b1
Siwadon Saosoong <siwadon.s@gmail.com>
2018-05-06 10:48:24 -0700

    Remove double quotes from stations.csv and regenerate it from 2017 data

--- public/data/ford_gobike/create-stations.sh
+++ public/data/ford_gobike/create-stations.sh
@@ -2,4 +2,4 @@ LIST=$(cat $1 | cut -d',' -f 4-7 && \
   cat $1 | cut -d',' -f 8-11)
 
 echo '"id","name","latitude","longitude"' > stations.csv
-echo "${LIST}" | sort -nk1 | uniq | sed '1,2d' >> stations.csv
+echo "${LIST}" | sed -e 's/\"//g' | sort -nk1 | uniq | sed '1,2d' >> stations.csv
