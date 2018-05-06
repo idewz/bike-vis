26997a12472d806dac9864a9ecd87f2a03596090
Siwadon Saosoong <siwadon.s@gmail.com>
2018-05-06 10:31:19 -0700

    Update sampler.sh to support input, output file with sampling option

--- public/data/ford_gobike/sampler.sh
+++ public/data/ford_gobike/sampler.sh
@@ -1,22 +1,43 @@
 #!/usr/bin/env sh
 
-# cut the head
-head -1 201803_fordgobike_tripdata.csv \
-  | cut -d',' -f 1-4,8,12-15 \
-  > mini.csv
+script_name=$(basename "$0")
 
-# shuffle 10,000 records
-cat 201803_fordgobike_tripdata.csv \
-  | cut -d',' -f 1-4,8,12-15 \
-  | gshuf -n 10000 \
-  >> mini.csv
+if [[ $# != 2 ]]; then
+    echo "Usage: $script_name INFILE OUTFILE [shuffle]" 
+      exit
+    fi
 
-# clean up
-sed -i '' -e 's/\"Male\"$/0/' mini.csv
-sed -i '' -e 's/\"Female\"$/1/' mini.csv
-sed -i '' -e 's/\"Other\"$/2/' mini.csv
-sed -i '' -e 's/\"\"$/-1/' mini.csv
+input_file=$1
+output_file=$2
+shuffle={$3:-true}
 
-sed -i '' -e 's/\"Subscriber\"/0/' mini.csv
-sed -i '' -e 's/\"Customer\"/1/' mini.csv
+if [ $shuffle = true ] ;
+then
+  # cut the head
+  head -1 $input_file \
+    | cut -d',' -f 1-4,8,12-15 \
+    > $output_file
+
+  echo "shuffling to $output_file"
+  cat $input_file \
+    | cut -d',' -f 1-4,8,12-15 \
+    | gshuf -n 10000 \
+    >> $output_file
+else
+
+  echo "copying to $output_file"
+  cat $input_file \
+    | cut -d',' -f 1-4,8,12-15 \
+    > $output_file
+fi
+
+echo "cleaning up fields"
+sed -i '' -e 's/\"Male\"/0/' $output_file
+sed -i '' -e 's/\"Female\"/1/' $output_file
+sed -i '' -e 's/\"Other\"/2/' $output_file
+sed -i '' -e 's/\"\"$/-1/' $output_file
+
+sed -i '' -e 's/\"Subscriber\"/0/' $output_file
+sed -i '' -e 's/\"Customer\"/1/' $output_file
+sed -i '' -e 's/\"//g' $output_file
 
