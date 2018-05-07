a517c124876b1213adc6b438f4fdb2965362c544
Siwadon Saosoong <siwadon.s@gmail.com>
2018-05-06 20:34:34 -0700

    Add top destinations

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -7,7 +7,10 @@ import MenuItem from 'material-ui/Menu/MenuItem';
 import TextField from 'material-ui/TextField';
 import Typography from 'material-ui/Typography';
 
+import countBy from 'lodash.countby';
+
 import MyMap from './MyMap';
+import { niceNumber } from '../utils';
 
 class MapContainer extends Component {
   constructor() {
@@ -45,8 +48,57 @@ class MapContainer extends Component {
     this.setState({ zoom });
   }
 
+  renderDestination(id, count) {
+    const { stations } = this.props;
+    const station = stations.find(s => s.id === id);
+
+    return (
+      <Grid container key={station.id} justify="space-between">
+        <Grid>
+          <Typography variant="body1">{station.name}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1">{niceNumber(count)} trips</Typography>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  renderDestinationList() {
+    const destTrips = this.props.trips.filter(
+      t => t.start_station.id === this.state.selectedId
+    );
+
+    let destCounts = countBy(destTrips, t => t.end_station.id);
+    let result = Object.keys(destCounts).map(function(key) {
+      return { id: Number(key), trips: destCounts[key] };
+    });
+
+    const top5 = result
+      .sort((a, b) => a.trips - b.trips)
+      .reverse()
+      .slice(0, 5);
+
+    return (
+      top5 && (
+        <div>
+          <Typography
+            variant="body2"
+            className={this.props.classes.destination}
+          >
+            Top 5 Destinations
+          </Typography>
+          {top5.map(s => this.renderDestination(s.id, s.trips))}
+        </div>
+      )
+    );
+  }
+
   render() {
     const { classes, stations } = this.props;
+    const station = this.props.stations.find(
+      e => e.id === this.state.selectedId
+    );
 
     return (
       <Grid container spacing={24}>
@@ -84,13 +136,18 @@ class MapContainer extends Component {
               </MenuItem>
             ))}
           </TextField>
-          {this.state.selectedId !== 0 && (
-            <Typography variant="headline">
-              {
-                this.props.stations.find(e => e.id === this.state.selectedId)
-                  .name
-              }
-            </Typography>
+          {station && (
+            <Grid container>
+              <Grid item xs={12}>
+                <Typography variant="headline">{station.name}</Typography>
+                <Typography variant="subheading">
+                  {station.area.name}
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                {this.renderDestinationList()}
+              </Grid>
+            </Grid>
           )}
         </Grid>
       </Grid>
@@ -107,6 +164,10 @@ MapContainer.propTypes = {
 
 const styles = theme => {
   return {
+    destination: {
+      marginTop: 16,
+      marginBottom: 8,
+    },
     textField: {
       width: 360,
     },
